8fb29ad8621716a5bb42d3668324b52138b0ca60
snsinfu <snsinfu@gmail.com>
2018-10-29 16:26:18 +0900

    linear_hash: Add test cases

--- tests/test_linear_hash.cc
+++ tests/test_linear_hash.cc
@@ -16,7 +16,15 @@ TEST_CASE("linear_hash - is default constructible and modulus is nonzero")
     CHECK(hash.modulus > 0);
 }
 
-TEST_CASE("linear_hash::operator() - computes linear hash")
+TEST_CASE("linear_hash::operator() - trivial zero points")
+{
+    md::linear_hash hash;
+
+    CHECK(hash(0, 0, 0) == 0);
+    CHECK(hash(hash.modulus, hash.modulus, hash.modulus) == 0);
+}
+
+TEST_CASE("linear_hash::operator() - computes expected hash value")
 {
     md::linear_hash hash;
 
@@ -27,9 +35,17 @@ TEST_CASE("linear_hash::operator() - computes linear hash")
 
     md::linear_hash const& const_hash = hash;
 
-    CHECK(const_hash(10, 100, 1000) == 7);
+    CHECK(const_hash(1, 0, 0) == 2);
+    CHECK(const_hash(0, 1, 0) == 3);
+    CHECK(const_hash(0, 0, 1) == 5);
+    CHECK(const_hash(1, 1, 1) == 10);
+
     CHECK(const_hash(1000, 10, 100) == 0);
+    CHECK(const_hash(1000, 100, 10) == 7);
+    CHECK(const_hash(10, 1000, 100) == 0);
     CHECK(const_hash(100, 1000, 10) == 5);
+    CHECK(const_hash(10, 100, 1000) == 7);
+    CHECK(const_hash(100, 10, 1000) == 5);
 }
 
 TEST_CASE("linear_hash::operator() - avoids 32-bit wraparound")
