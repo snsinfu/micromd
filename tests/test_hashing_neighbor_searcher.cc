1d8d43c567eca0704782c3b4a5c953749d550aff
snsinfu <snsinfu@gmail.com>
2018-10-30 16:59:34 +0900

    hashing_neighbor_searcher: Add test cases

--- tests/test_hashing_neighbor_searcher.cc
+++ tests/test_hashing_neighbor_searcher.cc
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <cmath>
-#include <iostream>
 #include <iterator>
 #include <random>
 #include <set>
@@ -32,7 +31,174 @@ TEST_CASE("hashing_neighbor_searcher - outputs nothing for empty points")
     CHECK(pairs.empty());
 }
 
-TEST_CASE("hashing_neighbor_searcher - finds correct neighbors")
+TEST_CASE("hashing_neighbor_searcher - finds neighbors on a line")
+{
+    md::scalar const cutoff_distance = 0.1;
+    md::vector const step_vector = {0.06571881, 0.00727112, 0.02298191};
+
+    // Generate points on a line
+    std::vector<md::point> points;
+    std::multiset<std::pair<md::index, md::index>> expected_pairs;
+
+    points.push_back({-1, -1, -1});
+    for (md::index i = 1; i < 50; i++) {
+        points.push_back(points.front() + md::scalar(i) * step_vector);
+        expected_pairs.emplace(i - 1, i);
+    }
+    CHECK(expected_pairs.size() == points.size() - 1);
+
+    // Test
+    std::multiset<std::pair<md::index, md::index>> pairs;
+    md::hashing_neighbor_searcher searcher(cutoff_distance, md::linear_hash{});
+    searcher.set_points(points);
+    searcher.search(std::inserter(pairs, pairs.end()));
+
+    CHECK(pairs.size() == expected_pairs.size());
+    CHECK(pairs == expected_pairs);
+}
+
+TEST_CASE("hashing_neighbor_searcher - finds neighbors on a 2D grid")
+{
+    md::scalar const cutoff_distance = 0.1;
+    md::scalar const spacing = 0.07;
+
+    // Generate 2D grid points
+    std::vector<md::point> points;
+
+    for (int x = -2; x <= 2; x++) {
+        for (int y = -2; y <= 2; y++) {
+            points.push_back({
+                spacing * x,
+                spacing * y,
+                0
+            });
+        }
+    }
+
+    // Brute-force compute neighbor pairs
+    std::multiset<std::pair<md::index, md::index>> expected_pairs;
+
+    for (md::index j = 0; j < points.size(); j++) {
+        for (md::index i = 0; i < j; i++) {
+            if (md::distance(points[i], points[j]) < cutoff_distance) {
+                expected_pairs.emplace(i, j);
+            }
+        }
+    }
+
+    // Test
+    std::multiset<std::pair<md::index, md::index>> pairs;
+    md::hashing_neighbor_searcher searcher(cutoff_distance, md::linear_hash{});
+    searcher.set_points(points);
+    searcher.search(std::inserter(pairs, pairs.end()));
+
+    CHECK(pairs.size() == expected_pairs.size());
+    CHECK(pairs == expected_pairs);
+}
+
+TEST_CASE("hashing_neighbor_searcher - finds neighbors on a 3D grid")
+{
+    md::scalar const cutoff_distance = 0.1;
+    md::scalar const spacing = 0.07;
+
+    // Generate a 3D grid points
+    std::vector<md::point> points;
+
+    for (int x = -2; x <= 2; x++) {
+        for (int y = -2; y <= 2; y++) {
+            for (int z = -2; z <= 2; z++) {
+                points.push_back({
+                    spacing * x,
+                    spacing * y,
+                    spacing * z
+                });
+            }
+        }
+    }
+
+    // Brute-force compute neighbor pairs
+    std::multiset<std::pair<md::index, md::index>> expected_pairs;
+
+    for (md::index j = 0; j < points.size(); j++) {
+        for (md::index i = 0; i < j; i++) {
+            if (md::distance(points[i], points[j]) < cutoff_distance) {
+                expected_pairs.emplace(i, j);
+            }
+        }
+    }
+
+    // Test
+    std::multiset<std::pair<md::index, md::index>> pairs;
+    md::hashing_neighbor_searcher searcher(cutoff_distance, md::linear_hash{});
+    searcher.set_points(points);
+    searcher.search(std::inserter(pairs, pairs.end()));
+
+    CHECK(pairs.size() == expected_pairs.size());
+    CHECK(pairs == expected_pairs);
+}
+
+TEST_CASE("hashing_neighbor_searcher - finds neighbors in a random walk path")
+{
+    md::scalar const cutoff_distance = 0.1;
+    md::scalar const spacing = 0.07;
+
+    // Generate a random walk path
+    std::vector<md::point> points;
+
+    std::mt19937_64 random;
+    std::normal_distribution<md::scalar> normal;
+
+    points.push_back({0, 0, 0});
+    for (int i = 1; i < 50; i++) {
+        md::vector step = {normal(random), normal(random), normal(random)};
+        step *= spacing / step.norm();
+        points.push_back(points.back() + step);
+    }
+
+    // Brute-force computed neighbor pairs
+    std::multiset<std::pair<md::index, md::index>> expected_pairs;
+
+    for (md::index j = 0; j < points.size(); j++) {
+        for (md::index i = 0; i < j; i++) {
+            if (md::distance(points[i], points[j]) < cutoff_distance) {
+                expected_pairs.emplace(i, j);
+            }
+        }
+    }
+
+    // Test
+    std::multiset<std::pair<md::index, md::index>> pairs;
+    md::hashing_neighbor_searcher searcher(cutoff_distance, md::linear_hash{});
+    searcher.set_points(points);
+    searcher.search(std::inserter(pairs, pairs.end()));
+
+    CHECK(pairs.size() == expected_pairs.size());
+    CHECK(pairs == expected_pairs);
+}
+
+TEST_CASE("hashing_neighbor_searcher - finds no false positives in a sparse system")
+{
+    md::scalar const cutoff_distance = 0.1;
+
+    std::vector<md::point> points;
+
+    for (int i = 0; i < 1000; i++) {
+        int const x = i % 10;
+        int const y = i / 10 % 10;
+        int const z = i / 10 / 10 % 10;
+
+        points.push_back({0.11 * x, 0.11 * y, 0.11 * z});
+    }
+
+    std::set<std::pair<md::index, md::index>> actual;
+    md::hashing_neighbor_searcher searcher(cutoff_distance, md::linear_hash{});
+    searcher.set_points(points);
+    searcher.search(std::inserter(actual, actual.end()));
+
+    CHECK(actual.empty());
+}
+
+TEST_CASE("hashing_neighbor_searcher - finds correct neighbors in a small example")
 {
     md::scalar const cutoff_distance = 1.0;
 
@@ -80,29 +246,7 @@ TEST_CASE("hashing_neighbor_searcher - finds correct neighbors")
     CHECK(actual == expected);
 }
 
-TEST_CASE("hashing_neighbor_searcher - detects no false positive")
-{
-    md::scalar const cutoff_distance = 0.1;
-
-    std::vector<md::point> points;
-
-    for (int i = 0; i < 1000; i++) {
-        int const x = i % 10;
-        int const y = i / 10 % 10;
-        int const z = i / 10 / 10 % 10;
-
-        points.push_back({0.11 * x, 0.11 * y, 0.11 * z});
-    }
-
-    std::set<std::pair<md::index, md::index>> actual;
-    md::hashing_neighbor_searcher searcher(cutoff_distance, md::linear_hash{});
-    searcher.set_points(points);
-    searcher.search(std::inserter(actual, actual.end()));
-
-    CHECK(actual.empty());
-}
-
-TEST_CASE("hashing_neighbor_searcher - normal distribution", "[.][slow]")
+TEST_CASE("hashing_neighbor_searcher - finds correct neighbors in a large cloud", "[.][slow]")
 {
     std::mt19937 random;
     std::normal_distribution<md::scalar> normal;
