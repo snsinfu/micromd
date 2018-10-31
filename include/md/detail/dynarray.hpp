b0f0ac6ffb8c105e5006af65dc9904f3304e5468
snsinfu <snsinfu@gmail.com>
2018-10-31 15:27:03 +0900

    dynarray: Split into a separate header

---
+++ include/md/detail/dynarray.hpp
@@ -0,0 +1,58 @@
+// Copyright snsinfu 2018.
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MD_DETAIL_DYNARRAY_HPP
+#define MD_DETAIL_DYNARRAY_HPP
+
+#include <vector>
+
+#include "../typedef.hpp"
+#include "../vendor/array_view.hpp"
+
+
+namespace md
+{
+    namespace detail
+    {
+        // dynarray_base is a type-erased base class of dynarray<T>.
+        class dynarray_base
+        {
+        public:
+            virtual ~dynarray_base() = default;
+
+            // resize resizes the internal storage to given size.
+            virtual void resize(md::index size) = 0;
+        };
+
+        // dynarray is a resizable array of Ts.
+        template<typename T>
+        class dynarray : public detail::dynarray_base
+        {
+        public:
+            dynarray(md::index size, T def)
+                : default_{def}, values_(size, def)
+            {
+            }
+
+            // resize resizes the internal storage to given size. Newly created
+            // elements are filled with the default value.
+            void resize(md::index size) override
+            {
+                values_.resize(size, default_);
+            }
+
+            // view returns a view into the array.
+            md::array_view<T> view()
+            {
+                return values_;
+            }
+
+        private:
+            T default_;
+            std::vector<T> values_;
+        };
+    }
+}
+
+#endif
