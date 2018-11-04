f1571f03687175920e009634cd692c043ab62d91
snsinfu <snsinfu@gmail.com>
2018-11-05 04:02:34 +0900

    simulation: Add basic brownian dynamics support

--- include/md/simulation/brownian_dynamics.hpp
+++ include/md/simulation/brownian_dynamics.hpp
@@ -8,6 +8,7 @@
 #include <cstdint>
 #include <functional>
 #include <random>
+#include <vector>
 
 #include "../basic_types.hpp"
 #include "../system.hpp"
@@ -24,6 +25,46 @@ namespace md
         std::uint64_t seed = 0;
         std::function<void(md::step)> callback;
     };
+
+    // simulate_brownian_dynamics simulates Brownian dynamics of the system.
+    inline void simulate_brownian_dynamics(md::system& system, md::brownian_dynamics_config config)
+    {
+        std::seed_seq seed {
+            unsigned(config.seed >> 32),
+            unsigned(config.seed)
+        };
+        std::mt19937_64 random(seed);
+        std::normal_distribution<md::scalar> normal;
+
+        // BAOAB limit scheme.
+
+        md::array_view<md::point> positions = system.view_positions();
+        md::array_view<md::scalar> mobilities = system.view_mobilities();
+        std::vector<md::vector> forces(system.particle_count());
+        std::vector<md::vector> weiners(system.particle_count());
+
+        for (md::step step = 0; step < config.steps; step++) {
+            system.compute_force(forces);
+
+            for (md::index i = 0; i < system.particle_count(); i++) {
+                md::scalar const mu_dt = config.timestep * mobilities[i];
+                md::scalar const sigma = std::sqrt(2 * config.temperature * mu_dt);
+                md::vector const weiner = {
+                    sigma * normal(random),
+                    sigma * normal(random),
+                    sigma * normal(random)
+                };
+
+                positions[i] += mu_dt * forces[i];
+                positions[i] += 0.5 * (weiner + weiners[i]);
+                weiners[i] = weiner;
+            }
+
+            if (config.callback) {
+                config.callback(step);
+            }
+        }
+    }
 }
 
 #endif
--- tests/simulation/test_brownian_dynamics.cc
+++ tests/simulation/test_brownian_dynamics.cc
@@ -1,4 +1,7 @@
+#include <memory>
+
 #include <md/basic_types.hpp>
+#include <md/forcefield.hpp>
 #include <md/system.hpp>
 #include <md/simulation/brownian_dynamics.hpp>
 
@@ -14,4 +17,54 @@ TEST_CASE("brownian_dynamics_config - defaults to sane configuration")
     CHECK(config.steps == 1);
     CHECK(config.seed == 0);
     CHECK(!config.callback);
+}
+
+TEST_CASE("simulate_brownian_dynamics - does nothing if steps is zero")
+{
+    md::system system;
+
+    system.add_particle();
+
+    md::brownian_dynamics_config config;
+    config.steps = 0;
+
+    md::simulate_brownian_dynamics(system, config);
+
+    CHECK(system.view_positions()[0].x == 0);
+    CHECK(system.view_positions()[0].y == 0);
+    CHECK(system.view_positions()[0].z == 0);
+}
+
+TEST_CASE("simulate_brownian_dynamics - can simulate frozen system")
+{
+    md::system system;
+
+    system.add_particle();
+
+    md::brownian_dynamics_config config;
+    config.temperature = 0;
+    config.steps = 100;
+
+    md::simulate_brownian_dynamics(system, config);
+
+    CHECK(system.view_positions()[0].x == 0);
+    CHECK(system.view_positions()[0].y == 0);
+    CHECK(system.view_positions()[0].z == 0);
+}
+
+TEST_CASE("simulate_brownian_dynamics - can simulate brownian motion")
+{
+    md::system system;
+
+    system.add_particle();
+
+    md::brownian_dynamics_config config;
+    config.temperature = 1;
+    config.steps = 100;
+
+    md::simulate_brownian_dynamics(system, config);
+
+    CHECK(system.view_positions()[0].x != 0);
+    CHECK(system.view_positions()[0].y != 0);
+    CHECK(system.view_positions()[0].z != 0);
 }
