b23442d6093372c0a5d221daf8adc56a94730cf2
snsinfu <snsinfu@gmail.com>
2018-11-05 04:47:26 +0900

    potential: Add lennard_jones_potential

---
+++ include/md/potential/lennard_jones_potential.hpp
@@ -0,0 +1,37 @@
+// Copyright snsinfu 2018.
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MD_POTENTIAL_LENNARD_JONES_POTENTIAL_HPP
+#define MD_POTENTIAL_LENNARD_JONES_POTENTIAL_HPP
+
+#include "../basic_types.hpp"
+
+
+namespace md
+{
+    struct lennard_jones_potential
+    {
+        md::scalar epsilon = 1;
+        md::scalar sigma = 1;
+
+        md::scalar evaluate_energy(md::vector r) const
+        {
+            md::scalar const u2 = sigma * sigma / r.squared_norm();
+            md::scalar const u6 = u2 * u2 * u2;
+
+            return epsilon * (u6 * u6 - u6 - u6);
+        }
+
+        md::vector evaluate_force(md::vector r) const
+        {
+            md::scalar const r2_inv = 1 / r.squared_norm();
+            md::scalar const u2 = sigma * sigma * r2_inv;
+            md::scalar const u6 = u2 * u2 * u2;
+
+            return 12 * epsilon * (u6 * u6 - u6) * r2_inv * r;
+        }
+    };
+}
+
+#endif
---
+++ tests/potential/test_lennard_jones_potential.cc
@@ -0,0 +1,95 @@
+#include <md/basic_types.hpp>
+#include <md/potential/lennard_jones_potential.hpp>
+
+#include <catch.hpp>
+
+
+TEST_CASE("lennard_jones_potential - uses defined default parameters")
+{
+    md::lennard_jones_potential pot;
+
+    CHECK(pot.epsilon == 1);
+    CHECK(pot.sigma == 1);
+}
+
+TEST_CASE("lennard_jones_potential - takes minimum energy at sigma")
+{
+    md::lennard_jones_potential pot;
+
+    pot.epsilon = 1.23;
+    pot.sigma = 4.56;
+
+    SECTION("x direction")
+    {
+        md::vector const r = {pot.sigma, 0, 0};
+        md::scalar const energy = pot.evaluate_energy(r);
+        md::vector const force = pot.evaluate_force(r);
+
+        CHECK(energy == Approx(-pot.epsilon));
+        CHECK(force.x == Approx(0));
+        CHECK(force.y == Approx(0));
+        CHECK(force.z == Approx(0));
+    }
+
+    SECTION("y direction")
+    {
+        md::vector const r = {0, pot.sigma, 0};
+        md::scalar const energy = pot.evaluate_energy(r);
+        md::vector const force = pot.evaluate_force(r);
+
+        CHECK(energy == Approx(-pot.epsilon));
+        CHECK(force.x == Approx(0));
+        CHECK(force.y == Approx(0));
+        CHECK(force.z == Approx(0));
+    }
+
+    SECTION("z direction")
+    {
+        md::vector const r = {0, 0, pot.sigma};
+        md::scalar const energy = pot.evaluate_energy(r);
+        md::vector const force = pot.evaluate_force(r);
+
+        CHECK(energy == Approx(-pot.epsilon));
+        CHECK(force.x == Approx(0));
+        CHECK(force.y == Approx(0));
+        CHECK(force.z == Approx(0));
+    }
+}
+
+TEST_CASE("lennard_jones_potential::evaluate_force - computes correctly directed force")
+{
+    md::lennard_jones_potential pot;
+
+    // Repulsive for r < sigma, attractive for r > sigma.
+    pot.sigma = 1.5;
+
+    CHECK(pot.evaluate_force({1, 0, 0}).x > 0);
+    CHECK(pot.evaluate_force({0, 1, 0}).y > 0);
+    CHECK(pot.evaluate_force({0, 0, 1}).z > 0);
+
+    CHECK(pot.evaluate_force({2, 0, 0}).x < 0);
+    CHECK(pot.evaluate_force({0, 2, 0}).y < 0);
+    CHECK(pot.evaluate_force({0, 0, 2}).z < 0);
+}
+
+TEST_CASE("lennard_jones_potential - computes correct interaction")
+{
+    md::lennard_jones_potential pot;
+
+    pot.epsilon = 1.2;
+    pot.sigma = 3.0;
+
+    md::vector r;
+
+    r = {1, 2, 3};
+    CHECK(pot.evaluate_energy(r) == Approx(-0.552912));
+    CHECK(pot.evaluate_force(r).x == Approx(-0.200664));
+    CHECK(pot.evaluate_force(r).y == Approx(-0.401327));
+    CHECK(pot.evaluate_force(r).z == Approx(-0.601991));
+
+    r = {0, 1, 2};
+    CHECK(pot.evaluate_energy(r) == Approx(26.8179));
+    CHECK(pot.evaluate_force(r).x == Approx(0));
+    CHECK(pot.evaluate_force(r).y == Approx(81.1590));
+    CHECK(pot.evaluate_force(r).z == Approx(162.318));
+}
