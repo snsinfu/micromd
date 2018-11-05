742cff4140ce8f10a1c54c618d8eb5214443e27b
snsinfu <snsinfu@gmail.com>
2018-11-06 03:32:23 +0900

    all: Add all.hpp

---
+++ include/md/all.hpp
@@ -0,0 +1,24 @@
+// Copyright snsinfu 2018.
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MD_ALL_HPP
+#define MD_ALL_HPP
+
+#include "basic_types.hpp"
+#include "forcefield.hpp"
+#include "system.hpp"
+
+#include "potential/constant_potential.hpp"
+#include "potential/harmonic_potential.hpp"
+#include "potential/lennard_jones_potential.hpp"
+#include "potential/power_law_potential.hpp"
+
+#include "forcefield/composite_forcefield.hpp"
+#include "forcefield/neighbor_pair_forcefield.hpp"
+#include "forcefield/sphere_surface_forcefield.hpp"
+
+#include "simulation/brownian_dynamics.hpp"
+#include "simulation/newtonian_dynamics.hpp"
+
+#endif
