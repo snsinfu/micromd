1d16bbb83368fc73b4702cf1259bd50e9898e792
snsinfu <snsinfu@gmail.com>
2018-11-06 02:45:52 +0900

    simulation: Add simulate_newtonian_dynamics

---
+++ include/md/simulation/newtonian_dynamics.hpp
@@ -0,0 +1,58 @@
+// Copyright snsinfu 2018.
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MD_SIMULATION_NEWTONIAN_DYMNAMICS_HPP
+#define MD_SIMULATION_NEWTONIAN_DYMNAMICS_HPP
+
+#include <cmath>
+#include <functional>
+#include <vector>
+
+#include "../basic_types.hpp"
+#include "../system.hpp"
+
+
+namespace md
+{
+    // Parameters for Newtonian dynamics simulation.
+    struct newtonian_dynamics_config
+    {
+        // Time discretization step.
+        md::scalar timestep = 1;
+
+        // Number of steps to simulate.
+        md::step steps = 1;
+
+        // Optional function called after each step.
+        std::function<void(md::step)> callback;
+    };
+
+    inline void simulate_newtonian_dynamics(md::system& system, md::newtonian_dynamics_config config)
+    {
+        std::vector<md::vector> forces(system.particle_count());
+
+        md::array_view<md::scalar const> masses = system.view_masses();
+        md::array_view<md::point> positions = system.view_positions();
+        md::array_view<md::vector> velocities = system.view_velocities();
+
+        // Velocity Verlet scheme.
+
+        for (md::step step = 0; step < config.steps; step++) {
+            md::scalar const timestep = config.timestep;
+
+            for (md::index i = 0; i < system.particle_count(); i++) {
+                velocities[i] += timestep / (2 * masses[i]) * forces[i];
+                positions[i] += timestep * velocities[i];
+            }
+
+            system.compute_force(forces);
+
+            for (md::index i = 0; i < system.particle_count(); i++) {
+                velocities[i] += timestep / (2 * masses[i]) * forces[i];
+            }
+        }
+    }
+}
+
+#endif
---
+++ tests/simulation/test_newtonian_dynamics.cc
@@ -0,0 +1,80 @@
+#include <memory>
+
+#include <md/basic_types.hpp>
+#include <md/forcefield.hpp>
+#include <md/system.hpp>
+
+#include <md/simulation/newtonian_dynamics.hpp>
+
+#include <catch.hpp>
+
+
+namespace
+{
+    class gravitational_forcefield : public md::forcefield
+    {
+    public:
+        md::scalar compute_energy(md::system const& system) override
+        {
+            md::scalar sum = 0;
+            for (md::point pos : system.view_positions()) {
+                sum += pos.z;
+            }
+            return sum;
+        }
+
+        void compute_force(md::system const&, md::array_view<md::vector> forces) override
+        {
+            for (md::vector& force : forces) {
+                force.z -= 1;
+            }
+        }
+    };
+}
+
+
+TEST_CASE("newtonian_dynamics_config - defaults to sane configuration")
+{
+    md::newtonian_dynamics_config config;
+
+    CHECK(config.timestep == 1);
+    CHECK(config.steps == 1);
+    CHECK(!config.callback);
+}
+
+TEST_CASE("simulate_newtonian_dynamics - does nothing if steps is zero")
+{
+    md::system system;
+    system.add_particle();
+    system.add_forcefield(std::make_shared<::gravitational_forcefield>());
+
+    md::newtonian_dynamics_config config;
+    config.steps = 0;
+
+    md::simulate_newtonian_dynamics(system, config);
+
+    CHECK(system.view_positions()[0].x == 0);
+    CHECK(system.view_positions()[0].y == 0);
+    CHECK(system.view_positions()[0].z == 0);
+}
+
+TEST_CASE("simulate_brownian_dynamics - can simulate newtonian dynamics")
+{
+    md::system system;
+    system.add_particle();
+    system.add_forcefield(std::make_shared<::gravitational_forcefield>());
+
+    md::newtonian_dynamics_config config;
+    config.timestep = 0.01;
+    config.steps = 100;
+
+    md::simulate_newtonian_dynamics(system, config);
+
+    CHECK(system.view_positions()[0].x == 0);
+    CHECK(system.view_positions()[0].y == 0);
+    CHECK(system.view_positions()[0].z == Approx(-0.5).epsilon(0.1));
+
+    CHECK(system.view_velocities()[0].x == 0);
+    CHECK(system.view_velocities()[0].y == 0);
+    CHECK(system.view_velocities()[0].z == Approx(-1.0).epsilon(0.1));
+}
