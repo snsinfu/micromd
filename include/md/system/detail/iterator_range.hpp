ca9cc537a0aa44885fd60b126cdd6acb56331fc7
snsinfu <snsinfu@gmail.com>
2018-11-07 03:18:11 +0900

    system: Add detail::iterator_range

---
+++ include/md/system/detail/iterator_range.hpp
@@ -0,0 +1,38 @@
+// Copyright snsinfu 2018.
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MD_SYSTEM_DETAIL_ITERATOR_RANGE_HPP
+#define MD_SYSTEM_DETAIL_ITERATOR_RANGE_HPP
+
+namespace md
+{
+    namespace detail
+    {
+        template<typename Iterator>
+        class iterator_range
+        {
+        public:
+            iterator_range(Iterator begin, Iterator end)
+                : begin_{begin}, end_{end}
+            {
+            }
+
+            Iterator begin() const
+            {
+                return begin_;
+            }
+
+            Iterator end() const
+            {
+                return end_;
+            }
+
+        private:
+            Iterator begin_;
+            Iterator end_;
+        };
+    }
+}
+
+#endif
