933df23c8e959ac3d6b64c12b7e1d5c175e9e62b
snsinfu <snsinfu@gmail.com>
2018-11-09 01:18:53 +0900

    tests: Add test_ellipsoid_surface_energy_conservation
    This integration test checks if ellipsoid_surface_forcefield correctly
    computes force distortion coming from non-uniform curvature. If it is
    correct the mechanical energy must be conserved in Newtonian dynamics
    simulations.

--- tests/depends.mk
+++ tests/depends.mk
@@ -108,6 +108,28 @@ forcefield/test_sphere_surface_forcefield.o: \
   ../include/md/forcefield/../forcefield.hpp \
   ../include/md/forcefield/../system.hpp \
   ../include/md/forcefield/../potential/constant_potential.hpp
+integration_tests/test_ellipsoid_surface_energy_conservation.o: \
+  integration_tests/test_ellipsoid_surface_energy_conservation.cc \
+  ../include/md/basic_types.hpp ../include/md/basic_types/array_view.hpp \
+  ../include/md/basic_types/point.hpp ../include/md/system.hpp \
+  ../include/md/forcefield.hpp ../include/md/system/attribute.hpp \
+  ../include/md/system/detail/attribute_table.hpp \
+  ../include/md/system/detail/../../basic_types.hpp \
+  ../include/md/system/detail/../attribute.hpp \
+  ../include/md/system/detail/array_erasure.hpp \
+  ../include/md/system/detail/iterator_range.hpp \
+  ../include/md/system/detail/sum_forcefield.hpp \
+  ../include/md/system/detail/../../forcefield.hpp \
+  ../include/md/forcefield/ellipsoid_surface_forcefield.hpp \
+  ../include/md/forcefield/../basic_types.hpp \
+  ../include/md/forcefield/../forcefield.hpp \
+  ../include/md/forcefield/../system.hpp \
+  ../include/md/forcefield/../potential/constant_potential.hpp \
+  ../include/md/forcefield/../potential/../basic_types.hpp \
+  ../include/md/potential/harmonic_potential.hpp \
+  ../include/md/simulation/newtonian_dynamics.hpp \
+  ../include/md/simulation/../basic_types.hpp \
+  ../include/md/simulation/../system.hpp
 main.o: main.cc
 potential/test_softcore_potential.o: potential/test_softcore_potential.cc \
   ../include/md/basic_types.hpp ../include/md/basic_types/array_view.hpp \
---
+++ tests/integration_tests/test_ellipsoid_surface_energy_conservation.cc
@@ -0,0 +1,52 @@
+#include <md/basic_types.hpp>
+#include <md/system.hpp>
+#include <md/forcefield/ellipsoid_surface_forcefield.hpp>
+#include <md/potential/harmonic_potential.hpp>
+#include <md/simulation/newtonian_dynamics.hpp>
+
+#include <catch.hpp>
+
+
+namespace
+{
+    class ellipsoid_packing_forcefield
+        : public md::ellipsoid_surface_forcefield<ellipsoid_packing_forcefield>
+    {
+    public:
+        auto ellipsoid_outward_potential(md::system const&, md::index)
+        {
+            md::harmonic_potential harmonic;
+            harmonic.spring_constant = 1000;
+            return harmonic;
+        }
+    };
+}
+
+TEST_CASE("Energy conservation of particles in a ellipsoid")
+{
+    md::system system;
+
+    md::ellipsoid ellipsoid;
+    ellipsoid.semiaxis_x = 0.6;
+    ellipsoid.semiaxis_y = 0.4;
+    ellipsoid.semiaxis_z = 0.2;
+
+    md::particle_ref part = system.add_particle();
+    part.mass = 0.1;
+    part.position = ellipsoid.center;
+    part.velocity = {1, 2, 3};
+
+    ellipsoid_packing_forcefield forcefield;
+    forcefield.set_ellipsoid(ellipsoid);
+    system.add_forcefield(forcefield);
+
+    md::newtonian_dynamics_config config;
+    config.timestep = 1e-4;
+    config.steps = 100000;
+
+    md::scalar const initial_energy = system.compute_energy();
+    md::simulate_newtonian_dynamics(system, config);
+    md::scalar const final_energy = system.compute_energy();
+
+    CHECK(final_energy == Approx(initial_energy).epsilon(0.001));
+}
