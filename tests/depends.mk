c89408210b444e6e532724369288080098d868a3
snsinfu <snsinfu@gmail.com>
2019-06-10 04:25:46 +0900

    tests: Update depends.mk

--- tests/depends.mk
+++ tests/depends.mk
@@ -104,6 +104,11 @@ forcefield/test_neighbor_pair_forcefield.o: \
   ../include/md/system/detail/iterator_range.hpp \
   ../include/md/system/detail/sum_forcefield.hpp \
   ../include/md/system/detail/../../forcefield.hpp \
+  ../include/md/potential/harmonic_potential.hpp \
+  ../include/md/potential/../basic_types.hpp \
+  ../include/md/potential/softcore_potential.hpp \
+  ../include/md/potential/../misc/math.hpp \
+  ../include/md/potential/../misc/../basic_types.hpp \
   ../include/md/forcefield/neighbor_pair_forcefield.hpp \
   ../include/md/forcefield/../basic_types.hpp \
   ../include/md/forcefield/../forcefield.hpp \
@@ -112,7 +117,7 @@ forcefield/test_neighbor_pair_forcefield.o: \
   ../include/md/forcefield/detail/../../basic_types.hpp \
   ../include/md/forcefield/detail/../../misc/linear_hash.hpp \
   ../include/md/forcefield/detail/../../misc/neighbor_searcher.hpp \
-  ../include/md/forcefield/detail/../../misc/../basic_types.hpp \
+  ../include/md/potential/../misc/linear_hash.hpp \
   ../include/md/forcefield/detail/pair_potfun.hpp
 forcefield/test_composite_forcefield.o: \
   forcefield/test_composite_forcefield.cc ../include/md/basic_types.hpp \
@@ -183,12 +188,51 @@ forcefield/test_all_pair_forcefield.o: \
   ../include/md/system/detail/iterator_range.hpp \
   ../include/md/system/detail/sum_forcefield.hpp \
   ../include/md/system/detail/../../forcefield.hpp \
+  ../include/md/potential/harmonic_potential.hpp \
+  ../include/md/potential/../basic_types.hpp \
   ../include/md/forcefield/all_pair_forcefield.hpp \
   ../include/md/forcefield/../basic_types.hpp \
   ../include/md/forcefield/../forcefield.hpp \
   ../include/md/forcefield/../system.hpp \
   ../include/md/forcefield/detail/pair_potfun.hpp \
   ../include/md/forcefield/detail/../../basic_types.hpp
+integration_tests/test_persistence_length.o: \
+  integration_tests/test_persistence_length.cc \
+  ../include/md/basic_types.hpp ../include/md/basic_types/array_view.hpp \
+  ../include/md/basic_types/point.hpp ../include/md/basic_types/sfc.hpp \
+  ../include/md/basic_types/ziggurat.hpp ../include/md/system.hpp \
+  ../include/md/forcefield.hpp ../include/md/system/attribute.hpp \
+  ../include/md/system/particle.hpp \
+  ../include/md/system/../basic_types.hpp \
+  ../include/md/system/detail/attribute_table.hpp \
+  ../include/md/system/detail/../../basic_types.hpp \
+  ../include/md/system/detail/../attribute.hpp \
+  ../include/md/system/detail/array_erasure.hpp \
+  ../include/md/system/detail/iterator_range.hpp \
+  ../include/md/system/detail/sum_forcefield.hpp \
+  ../include/md/system/detail/../../forcefield.hpp \
+  ../include/md/forcefield/all_pair_forcefield.hpp \
+  ../include/md/forcefield/../basic_types.hpp \
+  ../include/md/forcefield/../forcefield.hpp \
+  ../include/md/forcefield/../system.hpp \
+  ../include/md/forcefield/detail/pair_potfun.hpp \
+  ../include/md/forcefield/detail/../../basic_types.hpp \
+  ../include/md/forcefield/sequential_pair_forcefield.hpp \
+  ../include/md/forcefield/sequential_triple_forcefield.hpp \
+  ../include/md/forcefield/detail/triple_potfun.hpp \
+  ../include/md/potential/cosine_bending_potential.hpp \
+  ../include/md/potential/../basic_types.hpp \
+  ../include/md/potential/softcore_potential.hpp \
+  ../include/md/potential/../misc/math.hpp \
+  ../include/md/potential/../misc/../basic_types.hpp \
+  ../include/md/potential/spring_potential.hpp \
+  ../include/md/simulation/brownian_dynamics.hpp \
+  ../include/md/simulation/../basic_types.hpp \
+  ../include/md/simulation/../system.hpp \
+  ../include/md/simulation/detail/brownian_simulator.hpp \
+  ../include/md/simulation/detail/../../basic_types.hpp \
+  ../include/md/simulation/detail/../../system.hpp \
+  ../include/md/simulation/detail/brownian_timestepper.hpp
 integration_tests/test_ellipsoid_surface_energy_conservation.o: \
   integration_tests/test_ellipsoid_surface_energy_conservation.cc \
   ../include/md/basic_types.hpp ../include/md/basic_types/array_view.hpp \
@@ -217,6 +261,12 @@ integration_tests/test_ellipsoid_surface_energy_conservation.o: \
   ../include/md/simulation/../basic_types.hpp \
   ../include/md/simulation/../system.hpp
 main.o: main.cc
+misc/test_math.o: misc/test_math.cc ../include/md/misc/math.hpp \
+  ../include/md/misc/../basic_types.hpp \
+  ../include/md/misc/../basic_types/array_view.hpp \
+  ../include/md/misc/../basic_types/point.hpp \
+  ../include/md/misc/../basic_types/sfc.hpp \
+  ../include/md/misc/../basic_types/ziggurat.hpp
 misc/test_linear_hash.o: misc/test_linear_hash.cc \
   ../include/md/misc/linear_hash.hpp
 misc/test_neighbor_searcher.o: misc/test_neighbor_searcher.cc \
@@ -232,7 +282,17 @@ potential/test_softcore_potential.o: potential/test_softcore_potential.cc \
   ../include/md/basic_types/point.hpp ../include/md/basic_types/sfc.hpp \
   ../include/md/basic_types/ziggurat.hpp \
   ../include/md/potential/softcore_potential.hpp \
-  ../include/md/potential/../basic_types.hpp
+  ../include/md/potential/../basic_types.hpp \
+  ../include/md/potential/../misc/math.hpp \
+  ../include/md/potential/../misc/../basic_types.hpp
+potential/test_polybell_potential.o: potential/test_polybell_potential.cc \
+  ../include/md/basic_types.hpp ../include/md/basic_types/array_view.hpp \
+  ../include/md/basic_types/point.hpp ../include/md/basic_types/sfc.hpp \
+  ../include/md/basic_types/ziggurat.hpp \
+  ../include/md/potential/polybell_potential.hpp \
+  ../include/md/potential/../basic_types.hpp \
+  ../include/md/potential/../misc/math.hpp \
+  ../include/md/potential/../misc/../basic_types.hpp
 potential/test_spring_potential.o: potential/test_spring_potential.cc \
   ../include/md/basic_types.hpp ../include/md/basic_types/array_view.hpp \
   ../include/md/basic_types/point.hpp ../include/md/basic_types/sfc.hpp \
