a74621e6dcf304e76b15c08d0775018ae3a9d6a9
snsinfu <snsinfu@gmail.com>
2019-08-30 14:21:12 +0900

    all: Add include-all header md.hpp
    Now the include directive is exactly the same in bundled and non-bundled
    installations:
    
      #include <md.hpp>
    
    md/all.hpp is kept as-is for backward compatibility. It will be removed
    in the future (v1.0.0) release.

---
+++ include/md.hpp
@@ -0,0 +1,10 @@
+// Copyright snsinfu 2019.
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MD_HPP
+#define MD_HPP
+
+#include "md/all.hpp"
+
+#endif
