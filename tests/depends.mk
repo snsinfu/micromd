f2732e97ff1d9632a97e0bfb6ce3a3e6d523d5ae
snsinfu <snsinfu@gmail.com>
2019-12-09 14:29:29 +0900

    tests: Update depends.mk

--- tests/depends.mk
+++ tests/depends.mk
@@ -117,6 +117,7 @@ forcefield/test_neighbor_pairwise_forcefield.o: \
   ../include/md/forcefield/detail/pair_potfun.hpp \
   ../include/md/forcefield/neighbor_pairwise_forcefield.hpp \
   ../include/md/misc/box.hpp \
+  ../include/md/misc/index_range.hpp \
   ../include/md/misc/linear_hash.hpp \
   ../include/md/misc/math.hpp \
   ../include/md/misc/neighbor_searcher.hpp \
@@ -214,6 +215,63 @@ integration_tests/test_ellipsoid_surface_energy_conservation.o: \
   ../include/md/system/particle.hpp \
   integration_tests/test_ellipsoid_surface_energy_conservation.cc
 integration_tests/test_persistence_length.o: \
+  ../include/md.hpp \
+  ../include/md/basic_types.hpp \
+  ../include/md/basic_types/array_view.hpp \
+  ../include/md/basic_types/point.hpp \
+  ../include/md/basic_types/sfc.hpp \
+  ../include/md/basic_types/ziggurat.hpp \
+  ../include/md/forcefield.hpp \
+  ../include/md/forcefield/bonded_pairwise_forcefield.hpp \
+  ../include/md/forcefield/bonded_triplewise_forcefield.hpp \
+  ../include/md/forcefield/bruteforce_pairwise_forcefield.hpp \
+  ../include/md/forcefield/composite_forcefield.hpp \
+  ../include/md/forcefield/detail/field_potfun.hpp \
+  ../include/md/forcefield/detail/neighbor_list.hpp \
+  ../include/md/forcefield/detail/neighbor_list_heuristics.hpp \
+  ../include/md/forcefield/detail/pair_potfun.hpp \
+  ../include/md/forcefield/detail/triple_potfun.hpp \
+  ../include/md/forcefield/ellipsoid_surface_forcefield.hpp \
+  ../include/md/forcefield/neighbor_pairwise_forcefield.hpp \
+  ../include/md/forcefield/plane_surface_forcefield.hpp \
+  ../include/md/forcefield/point_source_forcefield.hpp \
+  ../include/md/forcefield/sphere_surface_forcefield.hpp \
+  ../include/md/misc/box.hpp \
+  ../include/md/misc/index_range.hpp \
+  ../include/md/misc/linear_hash.hpp \
+  ../include/md/misc/math.hpp \
+  ../include/md/misc/neighbor_searcher.hpp \
+  ../include/md/misc/nsearch_detail/math.hpp \
+  ../include/md/misc/nsearch_detail/search_grid.hpp \
+  ../include/md/potential/constant_potential.hpp \
+  ../include/md/potential/cosine_bending_potential.hpp \
+  ../include/md/potential/cutoff_potential.hpp \
+  ../include/md/potential/detail/detection.hpp \
+  ../include/md/potential/diff_potential.hpp \
+  ../include/md/potential/harmonic_potential.hpp \
+  ../include/md/potential/lennard_jones_potential.hpp \
+  ../include/md/potential/scaled_potential.hpp \
+  ../include/md/potential/semispring_potential.hpp \
+  ../include/md/potential/soft_lennard_jones_potential.hpp \
+  ../include/md/potential/soft_wca_potential.hpp \
+  ../include/md/potential/softcore_potential.hpp \
+  ../include/md/potential/softwell_potential.hpp \
+  ../include/md/potential/spring_potential.hpp \
+  ../include/md/potential/sum_potential.hpp \
+  ../include/md/potential/wca_potential.hpp \
+  ../include/md/potential/wrapped_potential.hpp \
+  ../include/md/simulation/brownian_dynamics.hpp \
+  ../include/md/simulation/detail/brownian_simulator.hpp \
+  ../include/md/simulation/detail/brownian_timestepper.hpp \
+  ../include/md/simulation/newtonian_dynamics.hpp \
+  ../include/md/system.hpp \
+  ../include/md/system/attribute.hpp \
+  ../include/md/system/detail/array_erasure.hpp \
+  ../include/md/system/detail/attribute_table.hpp \
+  ../include/md/system/detail/iterator_range.hpp \
+  ../include/md/system/detail/sum_forcefield.hpp \
+  ../include/md/system/detail/type_hash.hpp \
+  ../include/md/system/particle.hpp \
   integration_tests/test_persistence_length.cc
 main.o: \
   main.cc
